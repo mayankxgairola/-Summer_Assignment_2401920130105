2539fd4b6430ab0da2845da8a1d4a61c0e89692a
Mayank Gairola <mayankxgairola@gmail.com>
2026-06-01 20:57:43 +0530

    Create Two Sum.cpp

---
+++ Week_1/Day_1/Two Sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int>mpp;
+
+        for(int i = 0; i < nums.size(); i++) {
+            int remaining = target - nums[i];
+
+            if(mpp.find(remaining) != mpp.end()) return {mpp[remaining], i};
+
+            mpp[nums[i]] = i;
+        }
+
+        return {-1, -1};
+    }
+};
