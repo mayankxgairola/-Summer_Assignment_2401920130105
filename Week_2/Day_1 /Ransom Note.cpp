619ee34f1d5b45b43444cda769c8e6fe38ee7a16
Mayank Gairola <mayankxgairola@gmail.com>
2026-06-08 21:40:01 +0530

    Create Ransom Note.cpp

---
+++ Week_2/Day_1 /Ransom Note.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int>v(26, 0);
+
+        for(int i = 0; i < magazine.size(); i++) {
+            v[magazine[i] - 'a']++;
+        }
+
+        for(int i = 0; i < ransomNote.size(); i++) {
+            v[ransomNote[i] - 'a']--;
+        }
+
+        for(int i = 0; i < 26; i++) {
+            if(v[i] < 0) return false;
+        }
+
+        return true;
+    }
+};
