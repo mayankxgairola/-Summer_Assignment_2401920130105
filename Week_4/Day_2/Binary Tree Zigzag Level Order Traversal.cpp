f45ae258f0cfa0b9b1ea958e6a8eb8024f2870de
Mayank Gairola <mayankxgairola@gmail.com>
2026-06-24 00:37:17 +0530

    Create Binary Tree Zigzag Level Order Traversal.cpp

---
+++ Week_4/Day_2/Binary Tree Zigzag Level Order Traversal.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>>ans;
+
+        if(root == NULL)return ans;
+
+        queue<TreeNode*>q;
+        q.push(root);
+        int ct = 1;
+
+        while(!q.empty()) {
+            int s = q.size();
+            vector<int>v;
+
+            for(int i = 0; i < s ; i++){
+                TreeNode *node = q.front();
+                q.pop();
+
+                if(node->left != NULL) q.push(node->left);
+                if(node->right != NULL) q.push(node->right);
+
+                v.push_back(node->val);
+            }
+            if(ct % 2 == 0) reverse(v.begin(), v.end());
+            ans.push_back(v);
+            ct++;
+        }
+        return ans;
+    }
+};
