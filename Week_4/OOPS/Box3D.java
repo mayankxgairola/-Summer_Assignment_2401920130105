68b9797b96febb086e9d7f4fd1da2a692b8c2088
Mayank Gairola <mayankxgairola@gmail.com>
2026-06-28 12:16:18 +0530

    Create Box3D.java

---
+++ Week_4/OOPS/Box3D.java
@@ -0,0 +1,14 @@
+class Box3D extends Box {
+
+    double height;
+
+    Box3D(double length, double breadth, double height) {
+        super(length, breadth);
+        this.height = height;
+    }
+
+    double volume() {
+        return length * breadth * height;
+    }
+
+}
