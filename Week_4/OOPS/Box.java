84d8d318aec60b139f80bbf3cec00893155331f3
Mayank Gairola <mayankxgairola@gmail.com>
2026-06-28 12:15:41 +0530

    Create Box.java

---
+++ Week_4/OOPS/Box.java
@@ -0,0 +1,15 @@
+class Box {
+
+    double length;
+    double breadth;
+
+    Box(double length, double breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    double area() {
+        return length * breadth;
+    }
+
+}
