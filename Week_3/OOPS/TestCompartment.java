631f8da7ec1e74947ca8914a4eea5734f6828fe8
Mayank Gairola <mayankxgairola@gmail.com>
2026-06-28 12:10:32 +0530

    Create TestCompartment.java

---
+++ Week_3/OOPS/TestCompartment.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+
+public class TestCompartment {
+
+    public static void main(String[] args) {
+
+        Compartment[] coach = new Compartment[10];
+        Random r = new Random();
+
+        for (int i = 0; i < coach.length; i++) {
+
+            int n = r.nextInt(4) + 1;
+
+            switch (n) {
+                case 1:
+                    coach[i] = new FirstClass();
+                    break;
+
+                case 2:
+                    coach[i] = new Ladies();
+                    break;
+
+                case 3:
+                    coach[i] = new General();
+                    break;
+
+                case 4:
+                    coach[i] = new Luggage();
+                    break;
+            }
+        }
+
+        System.out.println("Railway Coach Details:\n");
+
+        for (int i = 0; i < coach.length; i++) {
+            System.out.println("Compartment " + (i + 1) + " : " + coach[i].notice());
+        }
+    }
+}
