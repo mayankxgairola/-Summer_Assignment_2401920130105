23c6383f3ae2c25e35efed9fc8da7c81d23716de
Mayank Gairola <mayankxgairola@gmail.com>
2026-06-28 12:14:12 +0530

    Create Outer.java

---
+++ Week_4/OOPS/Outer.java
@@ -0,0 +1,15 @@
+class Outer {
+
+    void display() {
+        System.out.println("Display function of Outer class");
+    }
+
+    class Inner {
+
+        void display() {
+            System.out.println("Display function of Inner class");
+        }
+
+    }
+
+}
