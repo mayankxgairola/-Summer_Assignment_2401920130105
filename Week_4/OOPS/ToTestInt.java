46e46f3f8a718fd77637eda67c61883c62944d81
Mayank Gairola <mayankxgairola@gmail.com>
2026-06-28 12:13:53 +0530

    Create ToTestInt.java

---
+++ Week_4/OOPS/ToTestInt.java
@@ -0,0 +1,12 @@
+public class ToTestInt {
+
+    public static void main(String[] args) {
+
+        Arithmetic obj = new Arithmetic();
+
+        System.out.println("Square of 5 = " + obj.square(5));
+        System.out.println("Square of 10 = " + obj.square(10));
+
+    }
+
+}
